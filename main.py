9b4b53e779058f62aa97e1b46cc0762050d2b53a
Hussein Omar <husseinomar6190@gmail.com>
2022-04-22 11:00:40 +0300

    adding color extractor module called colorgram

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import random
+import colorgram
 
+# Extract 6 colors from an image.
+colors = colorgram.extract('colors_img.jpg', 6)
+rgb_colors = [x.rgb for x in colors]
+print(rgb_colors)
 numbers = [n for n in range(101)]
 print(numbers)
 
